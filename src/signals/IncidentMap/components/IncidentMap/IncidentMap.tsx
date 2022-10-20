53cc5658ae96c1d93eed7efb6e25ac81e77c063e
Oscar <bakkeroscar@gmail.com>
2022-10-20 14:37:44 +0200

    [SIG-4672] react albus refactor (#2327)
    * Add incident wizard steps logic.
    * Fix url basename.
    * Add fixes and tests.
    * Fix tests.
    * Fix coverage.
    * Remove react-albus from package.json.
    * Remove comment.
    * Add pr suggestions.

--- src/signals/IncidentMap/components/IncidentMap/IncidentMap.tsx
+++ src/signals/IncidentMap/components/IncidentMap/IncidentMap.tsx
@@ -4,9 +4,8 @@ import { useCallback, useEffect, useState, useRef } from 'react'
 
 import { ViewerContainer } from '@amsterdam/arm-core'
 import type { FeatureCollection } from 'geojson'
-import type { Map as MapType, LatLngLiteral } from 'leaflet'
-
 import { useFetch } from 'hooks'
+import type { Map as MapType, LatLngLiteral } from 'leaflet'
 import configuration from 'shared/services/configuration/configuration'
 import { dynamicIcon } from 'shared/services/configuration/map-markers'
 import MAP_OPTIONS from 'shared/services/configuration/map-options'
